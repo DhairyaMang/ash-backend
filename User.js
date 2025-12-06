1992dc909ec476b73bbeabd62a4fe4b04649eb67
DhairyaMang <133119066+DhairyaMang@users.noreply.github.com>
2025-12-06 22:17:17 +0530

    Add files via upload

---
+++ User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ConnectedAccountSchema = new mongoose.Schema(
+  {
+    accountId: String,
+    username: String,
+    accessToken: String,
+    refreshToken: String
+  },
+  { _id: false }
+);
+
+const CryptoSchema = new mongoose.Schema(
+  {
+    network: String,
+    address: String
+  },
+  { _id: false }
+);
+
+const UserSchema = new mongoose.Schema(
+  {
+    discordId: { type: String, required: true, index: true, unique: true },
+    connectedAccounts: {
+      instagram: { type: ConnectedAccountSchema, default: null },
+      tiktok: { type: ConnectedAccountSchema, default: null }
+    },
+    paymentMethods: {
+      gpay: { type: String, default: null },
+      paypal: { type: String, default: null },
+      crypto: { type: CryptoSchema, default: null }
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
